477283405d9ad9f07036a616bc9b4b8f28244422
Rob Hawkins <rhawkins@dosomething.org>
2011-04-29 09:14:03 -0500

    adding projects micro to source control

---
+++ sites/all/micro/projects/codes.js
@@ -0,0 +1,16 @@
+var sportsCode = '0';
+var artsCode = '1';
+var techCode = '2';
+
+var animalCode = '0';
+var disasterCode = '1';
+var discriminationCode = '2';
+var educationCode = '3';
+var environmentCode = '4';
+var healthCode = '5';
+var hivCode = '6';
+var internationalCode = '7';
+var povertyCode = '8';
+var violenceCode = '9';
+var warCode = '10';
+

092b8f67f447235020d37637364f91562acaf483
Miles Ulrich <mulrich@dosomething.org>
2011-07-10 19:52:27 -0500

    theming scavenger hunt report backs

--- sites/all/themes/dosomething/templates/campaign_hunt_2011_form.tpl.php
+++ sites/all/themes/dosomething/templates/campaign_hunt_2011_form.tpl.php
@@ -4,13 +4,19 @@ list($team,$postal_code,$email_or_cell) = explode('HnzMneI', urldecode($_GET['te
 $selected_day = urldecode($_GET['selected_day']);
 $challenge = urldecode($_GET['challenge']);
 
+$heading = 'Day '.$selected_day.' - '.$challenge;
+
 $form['buttons']['submit']['#value'] = 'submit';
-$form['title']['#value'] = $team;
-$form['locations'][0]['postal_code']['#value'] = $postal_code;
+if (arg(2) !='edit') {
+  $form['title']['#value'] = $team;
+  $form['locations'][0]['postal_code']['#value'] = $postal_code;
+}
+
 $form['buttons']['submit']['#attributes'] = array('class' => 'button button-medium shadow rounded');
 
-print '<h2>'.'Day '.$selected_day.' - '.$challenge.'</h2>';
+print '<h2>'.$heading.'</h2>';
 print '<h3>'.$team.'</h3>';
+print drupal_render($form['field_which_challenge']);
 print drupal_render($form['group_show_us_what_you_did']);
 print drupal_render($form['field_campaign_number_of_people']);
 print drupal_render($form['field_campaign_essay_how']);
---
+++ sites/all/themes/dosomething/templates/node-campaign_hunt_2011.tpl.php
@@ -0,0 +1,93 @@
+<?php
+  drupal_add_js(path_to_theme() . '/js/jcarousel/lib/jquery.jcarousel.min.js');
+  drupal_add_css(path_to_theme() . '/js/jcarousel/style.css');
+  drupal_add_css(path_to_theme() . '/js/jcarousel/skins/dosomething/skin.css');
+?>
+<script type="text/javascript">
+$(document).ready(function() {
+  $('#project-carousel').jcarousel();
+});
+</script>
+
+<div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix">
+  <h3><?=$field_campaign_project_type[0]['value'];?></h3>
+  <a href="/scavenger-hunt/leaderboard?team_name=<?=$title;?>&zip=<?=$location['postal_code'];?>"><img src="/sites/all/micro/hunt/logo.jpg" style="height: 75px;"/></a>
+  <?php if ($unpublished): ?>
+    <div class="unpublished"><?php print t('Unpublished'); ?></div>
+  <?php endif; ?>
+
+  <div class="content">
+    <div class="project-media">
+    <?php if ($field_campaign_pictures && $field_campaign_pictures[0]['filepath']):
+       $first_photo = array_shift($field_campaign_pictures);
+       $full_size = image_get_info('/var/www/html/'.$first_photo['filepath']);
+       $resized_path = imagecache_create_path('project_highlighted_photo', $first_photo['filepath']);
+       $resized = image_get_info('/var/www/html/'.$resized_path);
+       $full_size['file_size'] = 0;
+       $resized['file_size'] = 0;
+       //$main_photo = $first_photo['filepath'];
+       //if (! array_identical($full_size, $resized)) {
+       //  $main_photo = $resized_path;
+       //}
+       $main_photo = $resized_path;
+       /*dpm($first_photo);
+       dpm($resized_path);
+       dpm($resized);
+       dpm($full_size);*/
+       ?>
+       <a class="project-photo-wrapper" rel="lightbox[projectphotos]" title="<?=$first_photo['title'];?>" href="/<?=$first_photo['filepath'];?>">
+         <img src="/<?=$main_photo;?>"/></a>
+      <? if (sizeof($field_campaign_pictures)): ?>
+      <ul id="project-carousel" class="jcarousel-skin-dosomething">
+        <?php foreach($field_campaign_pictures as $photo) : ?>
+          <li><?php print $photo['view']; ?></li>
+        <?php endforeach; ?>
+      </ul>
+    <?php endif; endif; ?>
+    </div>
+
+    <?php if ($field_campaign_video[0]['embed']):?>
+    <br/><a href="#video">Check out our videos!</a>
+    <?php endif;?>
+
+
+    <div class="box blue">
+      <h2>how we tackled the challenge:</h2>
+      <?php print check_markup($field_campaign_essay_how[0]['value']); ?>
+    </div>
+
+    <div class="box">
+    <h2>why it's important:</h2>
+      <?php print check_markup($field_campaign_essay_why[0]['value']); ?>
+    </div>
+
+    <div class="box">
+    <h2>the biggest challenge:</h2>
+      <?php print check_markup($field_campaign_essay_challenge[0]['value']); ?>
+    </div>
+
+    <div class="box">
+    <h2>how you can get involved:</h2>
+      <?php print check_markup($field_others_involved[0]['value']); ?>
+    </div>
+     <?php
+        if ($field_campaign_video && $field_campaign_video[0]['embed']) { ?>
+          <div class="box">
+          <h2 id="video">videos:</h2>
+        <?
+          foreach ($field_campaign_video as $video) {
+            if ($video['view']) {
+              print $video['view'];
+            } else {
+              print '<a target="_blank" href="'.$video['embed'].'">'.$video['embed'].'</a>';
+            }
+          } ?>
+        </div>
+    <?  } ?>
+
+
+  </div>
+
+  <?php print $links; ?>
+
+</div> <!-- /.node -->

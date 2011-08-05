ece45952921c5e6f834d9c19cf58d8c6038e8568
Miles Ulrich <mulrich@dosomething.org>
2011-08-05 10:40:57 -0500

    projects map search uses projects api

--- sites/all/themes/dosomething/templates/page-api-projects.tpl.php
+++ sites/all/themes/dosomething/templates/page-api-projects.tpl.php
@@ -4,17 +4,23 @@
 $view_name = 'api_projects';
 $display_id = 'feed_1';
 
-$prefix = '<?xml version="1.0" encoding="utf-8"?><ds-api>';
-$suffix = '</ds-api>';
+$prefix = '<?xml version="1.0" encoding="utf-8"?><xml>';
+$suffix = '</xml>';
 
 $maxnum = trim($_GET['maxnum']);
 $key = trim($_GET['key']);
 $zip = $_GET['zip'];
 $country = $_GET['country'];
+$lat = $_GET['lat'];
+$lng = $_GET['lng'];
 $province = 'all';
 if (isset($_GET['province'])) {
   $province = $_GET['province'];
 }
+if (! isset($country)) {
+  $country = 'US';
+}
+
 $cause = 'all';
 if (isset ($_GET['cause'])) {
   $cause = $_GET['cause'];
@@ -45,7 +51,9 @@ if (isset($maxnum) && $maxnum && preg_match('/^[0-9]+$/', $maxnum)) {
 }
 
 $key_valid = 0;
-if ($key) {
+if ($key && $key == '11276fce3cf6ea958c0f842934802121') {
+  $key_valid = 1;
+} elseif ($key) {
   $key_object = db_fetch_object(db_query("SELECT n.nid,field_query_count_value as 'count' FROM node n INNER JOIN content_type_api_key ctak ON ctak.nid=n.nid WHERE n.title='%s' AND n.type='api_key'", $key));
   if (isset($key_object)) {
     $nid = $key_object->nid;
@@ -58,14 +66,25 @@ if ($key) {
 if ($key_valid) {
   db_query("UPDATE content_type_api_key SET field_query_count_value=field_query_count_value+1 WHERE nid='%d'",$nid);
   $view = views_get_view($view_name);
-
- if (isset($zip) && isset($country)) {
+  if ($zip) {
     $filter = $view->get_item($display_id,'filter','distance');
     $filter['value']['postal_code'] = $zip;
     $filter['value']['country'] = $country;
     $filter['value']['search_distance'] = $distance;
     $view->set_item($display_id, 'filter', 'distance', $filter);
- }
+    $view->set_item($display_id, 'filter', 'distance_1', NULL);
+  } elseif ($province != 'all') {
+    //do nothing, province argument is passed to view below
+    $view->set_item($display_id, 'filter', 'distance_1', NULL);
+    $view->set_item($display_id, 'filter', 'distance', NULL);
+  } elseif ($lat && $lng) {
+    $filter = $view->get_item($display_id,'filter','distance_1');
+    $filter['value']['latitude'] = $lat;
+    $filter['value']['longitude'] = $lng;
+    $filter['value']['search_distance'] = $distance;
+    $view->set_item($display_id, 'filter', 'distance_1', $filter);
+    $view->set_item($display_id, 'filter', 'distance', NULL);
+  }
   
   $view->set_current_page($page);
   print $view->preview($display_id, array($cause, $province, $keyword));
--- sites/all/themes/dosomething/templates/page-projects-map.tpl.php
+++ sites/all/themes/dosomething/templates/page-projects-map.tpl.php
@@ -1,15 +1,17 @@
 <!DOCTYPE html>
 <html>
 <head>
-<link type="text/css" rel="stylesheet" media="all" href="http://www.dosomething.org/files/ctools/css/b32116845453fc3082b1af22f71fe155.css?c">
-<link type="text/css" rel="stylesheet" media="all" href="http://www.dosomething.org/files/css/css_5b0ce16a88c20ff8e9055b3eb8694547.css">
-<link type="text/css" rel="stylesheet" media="print" href="http://www.dosomething.org/files/css/css_33f27bce19ac7028e2e15beb429820df.css">
+
+<title><?php print $head_title; ?></title>
+<?php print $head; ?>
+<?php print $styles; ?>
+
 <meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
 <style type="text/css">
   html { height: 100% }
-  body { height: 100%; margin: 0px; padding: 0px }
+  body { height: 100%; margin: 0px; padding: 0px; background-color: white; }
   #map_canvas { height: 100% }
-  .result { width: 290px; min-height:50px; padding: 5px 5px 10px 5px}
+  .result { min-height:50px; padding: 5px 5px 10px 5px}
 </style>
 <script type="text/javascript"
     src="http://maps.google.com/maps/api/js?sensor=true">
@@ -18,40 +20,71 @@
 var max_infowindow_length = 100;
 var max_result_length = 500;
 var map = null;
+var geocoder = null;
 var markers = new Array();
 
-  function initialize() {
+function initialize() {
     // TODO: Need to pick a better center location to start with.
-    var latlng = new google.maps.LatLng(40.77, -74.0);
+    var latlng = new google.maps.LatLng(48.57479,-115.839844);
     var myOptions = {
-      zoom: 8,
+      zoom: 3,
       center: latlng,
       mapTypeId: google.maps.MapTypeId.ROADMAP
     };
-    map = new google.maps.Map(document.getElementById("map_canvas"),
-        myOptions); 
-}                  
+    map = new google.maps.Map(document.getElementById("map_canvas"), myOptions); 
+    google.maps.event.trigger(map, "resize");
+    geocoder = new google.maps.Geocoder();
+}
+
+function myclick(i) {
+  google.maps.event.trigger(markers[i], "click");
+}
 
 function getSelectedOption(selectBox) {
   return selectBox.options[selectBox.selectedIndex].value;
 }
 
-function buildSearchUrl(page) {
+function buildSearchUrl(page,zip) {
   cause = getSelectedOption(document.getElementById("filter-cause"));
   keyword = document.getElementById("search-keyword").value;
   zip = document.getElementById("postal-code").value;
   distance = document.getElementById("input-proximity").value; 
   province = getSelectedOption(document.getElementById("filter-state"))
   maxnum = 10 //TODO: Maybe more...?
-
-  // Assumes API can handle blank values. 
+  latlng = '';
+  if (! zip) {
+    center = map.getCenter();
+    latlng = '&lat='+center.lat()+'&lng='+center.lng();
+    distance = myGetRadius()*.6; //padding, make sure all results appear in viewable window
+  }
+  // Assumes API can handle blank values.
   // TODO: Should probably do some error checking here too.
-  var url = "http://www.dosomething.org/api/projects?key=[yourAPIkey]&cause="+cause+
-            "&zip="+zip+"&keyword="+keyword+"&distance="+distance+"&maxnum="+maxnum+"&page="+page;
-  //TODO: Uncomment me.
-  //return url;
+  var url = "http://www.dosomething.org/api/projects?key=11276fce3cf6ea958c0f842934802121&cause="+cause+
+            "&zip="+zip+"&keyword="+keyword+"&distance="+distance+"&maxnum="+maxnum+"&page="+page+latlng+"&province="+province;
+  //alert(url);
+  return url;
+  //return "http://www.dosomething.org/sites/all/micro/projects/projects-new";
+}
 
-  return "http://www.dosomething.org/sites/all/micro/projects/projects-new";
+function myGetRadius() {
+  bounds = map.getBounds();
+
+  center = bounds.getCenter();
+  ne = bounds.getNorthEast();
+
+  // r = radius of the earth in statute miles
+  var r = 3963.0;  
+
+  // Convert lat or lng from decimal degrees into radians (divide by 57.2958)
+  var lat1 = center.lat() / 57.2958; 
+  var lon1 = center.lng() / 57.2958;
+  var lat2 = ne.lat() / 57.2958;
+  var lon2 = ne.lng() / 57.2958;
+
+  // distance = circle radius from center to Northeast corner of bounds
+  var radius = r * Math.acos(Math.sin(lat1) * Math.sin(lat2) + 
+    Math.cos(lat1) * Math.cos(lat2) * Math.cos(lon2 - lon1));
+  return radius;
 }
 
 function ellipsize(text, size) {
@@ -120,8 +153,8 @@ function search() {
     infowindowText = ellipsize(see_it, max_infowindow_length);
 
     // InfoWindow html content
-    var content = "<div><h3>" + title + "</h3></div><div id=\"descrip_" + i + "\">" +
-        infowindowText + "</div><div><a href=\""+url+"\">Project Info</a></div>";
+    var content = "<div><h3><a href=\""+url+"\">" + title + "</a></h3></div><div id=\"descrip_" + i + "\">" +
+        infowindowText + "</div><div><a href=\""+url+"\">More details...</a></div>";
       
     var marker = new google.maps.Marker({
       position: new google.maps.LatLng(lat, lng),
@@ -135,11 +168,11 @@ function search() {
     result.setAttribute("class", "result");
     result.setAttribute("id", "result"+i);
     if (i % 2 == 0) 
-      result.setAttribute("style", "background-color:#DEF3F9");
+      result.setAttribute("style", "background-color:white");
     else
-      result.setAttribute("style", "background-color:#8ECCDE");
+      result.setAttribute("style", "background-color:#DEF3F9");
 
-    result.innerHTML = "<p><b><a href=\""+url+"\" style=\"color: darkblue\">"+title+"</a></b></p>";
+    result.innerHTML = "<p><b><a href=\"javascript:myclick("+i+")\" style=\"color: darkblue\">"+title+"</a></b></p>";
     result.innerHTML += "<p>"+resultText+"</p>";
 
     document.getElementById("results").appendChild(result);
@@ -174,7 +207,7 @@ function search() {
 					<input type="text" id="postal-code" name="postal-code" size="10" style="display:inline">
 					<label for="filter-cause">Cause</label>
 					<select id="filter-cause">
-						<option value="*">&lt;All&gt;</option>
+						<option value="all">&lt;All&gt;</option>
 						<option value="Animal Welfare">Animal Welfare</option>
 						<option value="Disaster Response And Relief">Disaster Response And Relief</option>
 						<option value="Discrimination">Discrimination</option>
@@ -189,7 +222,7 @@ function search() {
 					</select>
 					<label for="filter-state">State</label>
 					<select id="filter-state">
-						<option value="*">&lt;All&gt;</option>
+						<option value="all">&lt;All&gt;</option>
 						<option value="AK">AK</option>
 						<option value="AL">AL</option>
 						<option value="AR">AR</option>
@@ -259,7 +292,7 @@ function search() {
 			</div>
 		</form>
 	</div>
-	<div id="results" style="clear:both; padding-top:50px; width: 300px; overflow:auto">
+	<div id="results" style="clear:both; height: 400px; overflow:auto">
   <!--TODO: Additional stylizing of this div; maybe scrollbar on the div instead of on the page?-->
   <!--TODO: Dynamically add next/previous results page links-->
   </div>
